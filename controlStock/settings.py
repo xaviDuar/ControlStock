7fad0af0eb1b6b0cc79790f66c68ad785d563e1f
xaviduar <xaviiduartee@gmail.com>
2026-06-24 16:34:36 -0300

    creando las apps

--- controlStock/settings.py
+++ controlStock/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'inventory.apps.Inventoryconfig'
+    'core.apps.CoreConfig',
 ]
 
 MIDDLEWARE = [
@@ -103,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-ar'
 
 TIME_ZONE = 'UTC'
 
